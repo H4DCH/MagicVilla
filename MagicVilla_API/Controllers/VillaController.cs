155d8313924e729e3bdac1b3382263480ea669b1
Harry Duarte <harrychicoh@gmail.com>
2024-03-15 20:51:21 -0500

    Primer Commit

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,31 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/villa")]
+    [ApiController]
+    public class VillaController: ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa
+                {
+                    Id=2,
+                    Nombre="Prueba1",
+                    FechaCreacion = DateTime.Now
+                },
+                new Villa
+                {
+                    Id = 3,
+                    Nombre="Prueba2",
+                    FechaCreacion=DateTime.Now
+                }
+            };
+        }
+    }
+}
